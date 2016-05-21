eac59597d4e017c446c70bb2c620fe51d690a739
ravi <ravi@ravi@micropyramid.com>
2016-05-21 07:42:44 +0530

    get single currency rate

--- CurrencyPy/converter.py
+++ CurrencyPy/converter.py
@@ -9,7 +9,7 @@ class RateNotFoundError(Exception):
     pass
 
 
-class SourceNotReadyError(Exception):
+class RatesNotAvailableError(Exception):
     """
     Custome Exception when http://fixer.io/ is Down are not available for currency rates
     """
@@ -33,7 +33,7 @@ class Common:
 
 class CurrencyRates(Common):
 
-    def get_all_rates(self, base_cur, date_str=None):
+    def get_rates(self, base_cur, date_str=None):
 
         if date_str is None:
             date_str = 'latest'
@@ -46,6 +46,23 @@ class CurrencyRates(Common):
         response = requests.get(source_url, params=payload)
         if response.status_code == 200:
             rates = response.json().get('rates', {})
-            print type(rates)
             return rates
-        raise SourceNotReadyError("Currency Rates Source Not Ready")
+        raise RatesNotAvailableError("Currency Rates Source Not Ready")
+
+    def get_rate(self, base_cur, dest_cur, date_str=None):
+        if date_str is None:
+            date_str = 'latest'
+        else:
+            self._validate_date(date_str)
+            date_str = str(date_str)
+
+        payload = {'base': base_cur, 'symbols': dest_cur}
+        source_url = self._source_url() + date_str
+        response = requests.get(source_url, params=payload)
+        if response.status_code == 200:
+            print response.json()
+            rate = response.json().get('rates', {}).get(dest_cur)
+            return rate
+        raise RatesNotAvailableError("Currency Rates Source Not Ready")
+
+    # def convert(self, base_cur, dest_cur, amount, date_str=None)
