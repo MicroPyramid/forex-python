07289f54cf8ca2e36e0694ff212a23267f6cff2d
ravi <ravi@ravi@micropyramid.com>
2016-06-02 17:19:12 +0530

    doc notes for all bit coin functions

--- forex_python/bitcoin.py
+++ forex_python/bitcoin.py
@@ -3,10 +3,13 @@ import requests
 
 class BtcConverter(object):
     """
-    Get bit coin rates convert
+    Get bit coin rates and convertion
     """
 
     def get_latest_price(self, currency):
+        """
+        Get Lates price of one bitcoin to valid Currency 1BTC => X USD
+        """
         url = 'https://api.coindesk.com/v1/bpi/currentprice/{}.json'.format(currency)
         response = requests.get(url)
         if response.status_code == 200:
@@ -16,6 +19,9 @@ class BtcConverter(object):
         return None
 
     def get_previous_price(self, currency, date_obj):
+        """
+        Get Price for one bit coin on given date
+        """
         start = date_obj.strftime('%Y-%m-%d')
         end = date_obj.strftime('%Y-%m-%d')
         url = (
@@ -32,6 +38,9 @@ class BtcConverter(object):
         return None
 
     def get_previous_price_list(self, currency, start_date, end_date):
+        """
+        Get List of prices between two dates
+        """
         start = start_date.strftime('%Y-%m-%d')
         end = end_date.strftime('%Y-%m-%d')
         url = (
@@ -48,6 +57,9 @@ class BtcConverter(object):
         return {}
 
     def convert_to_btc(self, amount, currency):
+        """
+        Convert X amount to Bit Coins
+        """
         url = 'https://api.coindesk.com/v1/bpi/currentprice/{}.json'.format(currency)
         response = requests.get(url)
         if response.status_code == 200:
@@ -60,6 +72,9 @@ class BtcConverter(object):
         return None
 
     def convert_btc_to_cur(self, coins, currency):
+        """
+        Convert X bit coins to valid currency amount
+        """
         url = 'https://api.coindesk.com/v1/bpi/currentprice/{}.json'.format(currency)
         response = requests.get(url)
         if response.status_code == 200:
@@ -72,6 +87,9 @@ class BtcConverter(object):
         return None
 
     def convert_to_btc_on(self, amount, currency, date_obj):
+        """
+        Convert X amount to BTC based on given date rate
+        """
         start = date_obj.strftime('%Y-%m-%d')
         end = date_obj.strftime('%Y-%m-%d')
         url = (
@@ -91,6 +109,9 @@ class BtcConverter(object):
         return None
 
     def convert_btc_to_cur_on(self, coins, currency, date_obj):
+        """
+        Convert X BTC to valid currency amount based on given date
+        """
         start = date_obj.strftime('%Y-%m-%d')
         end = date_obj.strftime('%Y-%m-%d')
         url = (
@@ -110,4 +131,7 @@ class BtcConverter(object):
         return None
 
     def get_symbol(self):
+        """
+        Here is Unicode symbol for bit coin
+        """
         return "\u0E3F"
